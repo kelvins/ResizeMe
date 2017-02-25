63ae32fb9b5ce9b5f0cd69518fa80c98eed79147
Kelvin Salton do Prado <kelvinpfw@hotmail.com>
2017-02-25 00:20:45 -0300

    Validate minimum and maximum values

--- resize/static/js/script.js
+++ resize/static/js/script.js
@@ -16,6 +16,20 @@ document.getElementById("file").onchange = function () {
 
 function onWidthValueChanged() {
 
+	if(document.getElementById("width").value == "") {
+		return;
+	}
+
+	var currentWidth = document.getElementById("width").value;
+
+	if(currentWidth <= 0) {
+		document.getElementById("width").value = 1;
+	}
+
+	if(currentWidth > 5000) {
+		document.getElementById("width").value = 5000;
+	}
+
 	if(document.getElementById("preview_image").src != "") {
 
 	    var image = new Image();
@@ -26,6 +40,33 @@ function onWidthValueChanged() {
 
 	    var factor = (height*100)/width;
 
-		document.getElementById("height").value = parseInt(((factor*document.getElementById("width").value)/100), 10);
+	    var newHeight = parseInt(((factor*currentWidth)/100), 10);
+
+	    if(newHeight <= 0) {
+			document.getElementById("height").value = 1;
+		}else if(newHeight > 5000) {
+			document.getElementById("height").value = 5000;
+		}else {
+			document.getElementById("height").value = newHeight;
+		}
+	}
+}
+
+function onHeightValueChanged() {
+
+	if(document.getElementById("height").value == "") {
+		return;
+	}
+
+	var currentHeight = document.getElementById("height").value;
+
+	if(currentHeight <= 0) {
+		document.getElementById("height").value = 1;
+		return;
+	}
+
+	if(currentHeight > 5000) {
+		document.getElementById("height").value = 5000;
+		return;
 	}
 }
