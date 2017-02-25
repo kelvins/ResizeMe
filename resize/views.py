cbfdb736b3f412202e7cf052216c8adff3ffa59d
Kelvin Salton do Prado <kelvinpfw@hotmail.com>
2017-02-25 18:24:21 -0300

    Save the resized image on a temporary folder

--- resize/views.py
+++ resize/views.py
@@ -1,16 +1,18 @@
 from django.http import HttpResponse, HttpResponseRedirect, StreamingHttpResponse
 from django.core.files.base import ContentFile
 from django.template import loader
-import StringIO, os, mimetypes, random
+import os, random
 from .forms import UploadFileForm
-from django.conf import settings
+#from django.conf import settings
 from PIL import Image
 from wsgiref.util import FileWrapper
+import tempfile
 
 slogans = []
 slogans.append('Resize any image to any size with few clicks!')
 slogans.append('The simplest image resizing app!')
-slogans.append('Just load an image, set the width and height, and click on RESIZE ME!')
+slogans.append('Just load an image, set the width and height, select an image format and click on RESIZE ME!')
+slogans.append('This is an open source project created under the MIT license, feel free to contribute to the project on Github!')
 
 def index(request):
 
@@ -32,8 +34,8 @@ def index(request):
 			if filedata != None:
 
 				# Get the width and height
-				width  = request.POST['width']
-				height = request.POST['height']
+				width  = int(request.POST['width'])
+				height = int(request.POST['height'])
 
 				# If the width is invalid (it should never happen because it is already validated in HTML and Javascript)
 				if width <= 0 or width > 5000:
@@ -49,30 +51,31 @@ def index(request):
 				if image_format != "jpg" and image_format != "png" and image_format != "bmp":
 					image_format = "png"
 
+				# The "image.save(tempFile, image_format)" function uses "jpeg" as the image format
+				if image_format == "jpg":
+					image_format = "jpeg"
+
 				# Create an image object using the Pillow library
 				image = Image.open(filedata)
 
 				# Create a size variable using the width and height
-				size = int(width), int(height)
+				size = width, height
 
 				# Resize the image using the width and height defined by the user
 				image = image.resize(size, Image.ANTIALIAS)
-
-				# Create a "unique" file name
-				#magicNumber = random.randint(0, 9999999)
-				#filename = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S_%f') + "_" + str(magicNumber) + "." + str(image_format)
-				filename = "resized_image." + str(image_format)
-				filepath = settings.MEDIA_ROOT + "\\" + filename
+				
+				# Temp file (NamedTemporaryFile delete the file as soon as possible)
+				tempFile = tempfile.NamedTemporaryFile()
 
 				# Try to save the image in the MEDIA_ROOT
 				try:
-					image.save( filepath )
+					image.save(tempFile, image_format)
+					tempFile.seek(0)
 
-					file = os.path.basename(filepath)
-					chunk_size = 8192
-					response = StreamingHttpResponse(FileWrapper(open(filepath, 'rb'), chunk_size), content_type=mimetypes.guess_type(filepath)[0])
-					response['Content-Length'] = os.path.getsize(filepath)    
-					response['Content-Disposition'] = "attachment; filename=%s" % file
+					wrapper  = FileWrapper(tempFile)
+					response = StreamingHttpResponse(wrapper, 'image/%s' % image_format)
+					response['Content-Disposition'] = 'attachment; filename=resized_image.%s' % image_format
+
 					return response
 
 				except AttributeError:
