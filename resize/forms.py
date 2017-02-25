a6c61a00fb736e55bb66b4387a81596cc6573dec
Kelvin Salton do Prado <kelvinpfw@hotmail.com>
2017-02-25 10:37:37 -0300

    Added image format option

--- resize/forms.py
+++ resize/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    file = forms.ImageField()
-    width = forms.IntegerField()
-    height = forms.IntegerField()
+	file = forms.ImageField()
+	width = forms.IntegerField()
+	height = forms.IntegerField()
+
+	CHOICES = [
+		('jpg','jpg'),
+		('png','png'),
+		('bmp','bmp')
+	]
+
+	image_format = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect())
--- resize/views.py
+++ resize/views.py
@@ -15,22 +15,45 @@ def index(request):
 		form = UploadFileForm(request.POST, request.FILES)
 
 		if form.is_valid():
+
 			filedata = request.FILES['file']
-			width = request.POST['width']
-			height = request.POST['height']
-			image = Image.open(filedata)
-			size = int(width), int(height)
-			image = image.resize(size, Image.ANTIALIAS)
 
-			magicNumber = random.randint(0, 9999999)
-			filename = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S_%f') + "_" + str(magicNumber) + ".png"
+			if filedata != None:
 
-			try:
-				image.save('C:/Users/Kelvin/Desktop/Django/resizeme/resize/static/imgs/' + filename)
-			except AttributeError:
-				print("Couldn't save image {}".format(image))
+				width = request.POST['width']
+				height = request.POST['height']
+				image_format = request.POST['image_format']
+				print(image_format)
+				image = Image.open(filedata)
+				size = int(width), int(height)
+				image = image.resize(size, Image.ANTIALIAS)
 
-			return HttpResponseRedirect('static/imgs/' + filename)
+				magicNumber = random.randint(0, 9999999)
+				filename = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S_%f') + "_" + str(magicNumber) + "." + str(image_format)
+
+				try:
+					image.save('C:/Users/Kelvin/Desktop/Django/resizeme/resize/static/imgs/' + filename)
+				except AttributeError:
+					print("Couldn't save image {}".format(image))
+
+				return HttpResponseRedirect('static/imgs/' + filename)
+
+		else:
+
+			template = loader.get_template('resize/index.html')
+
+			slogans = []
+			slogans.append('Resize any image to any size with few clicks!')
+			slogans.append('The simplest image resizing app!')
+			slogans.append('Just load an image, set the width and height, and click on RESIZE ME!')
+
+			index = random.randint(0, len(slogans)-1)
+
+			context = {
+				'slogan': slogans[index],
+			}
+
+			return HttpResponse(template.render(context, request))
 
 	else:
 
