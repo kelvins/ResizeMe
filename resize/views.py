0ae781e09618fd5dc75ca5d36512399f9bf79dbb
Kelvin Salton do Prado <kelvinpfw@hotmail.com>
2017-02-24 23:54:34 -0300

    Resizing images correctly

--- resize/views.py
+++ resize/views.py
@@ -1,35 +1,45 @@
 #from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 import random
+import StringIO
+import datetime
+from PIL import Image
+from .forms import UploadFileForm
+from django.core.files.base import ContentFile
 
 def index(request):
 
 	if request.method == 'POST':
 
-		template = loader.get_template('resize/index.html')
+		form = UploadFileForm(request.POST, request.FILES)
 
-		slogans = []
-		slogans.append('Resize any image to any size with few clicks!')
-		slogans.append('The simplest image resizing app!')
-		slogans.append('Just load an image, set the width and height and click on RESIZE ME!')
+		if form.is_valid():
+			filedata = request.FILES['file']
+			width = request.POST['width']
+			height = request.POST['height']
+			image = Image.open(filedata)
+			size = int(width), int(height)
+			image = image.resize(size, Image.ANTIALIAS)
 
-		index = random.randint(0, len(slogans)-1)
+			magicNumber = random.randint(0, 9999999)
+			filename = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S_%f') + "_" + str(magicNumber) + ".png"
 
-		context = {
-			'slogan': slogans[index],
-		}
+			try:
+				image.save('C:/Users/Kelvin/Desktop/Django/resizeme/resize/static/imgs/' + filename)
+			except AttributeError:
+				print("Couldn't save image {}".format(image))
 
-		return HttpResponse(template.render(context, request))
+			return HttpResponseRedirect('static/imgs/' + filename)
 
 	else:
-		
+
 		template = loader.get_template('resize/index.html')
 
 		slogans = []
 		slogans.append('Resize any image to any size with few clicks!')
 		slogans.append('The simplest image resizing app!')
-		slogans.append('Just load an image, set the width and height and click on RESIZE ME!')
+		slogans.append('Just load an image, set the width and height, and click on RESIZE ME!')
 
 		index = random.randint(0, len(slogans)-1)
 
