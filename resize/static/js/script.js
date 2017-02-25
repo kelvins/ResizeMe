32bf55897e27cf41a3dec7af3d3310216af6e387
Kelvin Salton do Prado <kelvinpfw@hotmail.com>
2017-02-24 21:34:14 -0300

    Added the original size visualization

--- resize/static/js/script.js
+++ resize/static/js/script.js
@@ -1,11 +1,14 @@
 
 document.getElementById("file").onchange = function () {
-    var reader = new FileReader();
 
-    reader.onload = function (e) {
-        // get loaded data and render thumbnail.
-        document.getElementById("preview_image").src = e.target.result;
-    };
+	var reader = new FileReader;
+
+	reader.onload = function(e) {
+	    var image = new Image();
+	    image.src = e.target.result;
+        document.getElementById("preview_image").src = image.src;
+        document.getElementById("original_size").innerHTML = "Original Size: " + image.width + " X " + image.height;
+	};
 
     // read the image file as a data URL.
     reader.readAsDataURL(this.files[0]);
--- resize/views.py
+++ resize/views.py
@@ -1,10 +1,40 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import random
 
 def index(request):
-	template = loader.get_template('resize/index.html')
-	context = {
-		'name': 'Django',
-	}
-	return HttpResponse(template.render(context, request))
+
+	if request.method == 'POST':
+
+		template = loader.get_template('resize/index.html')
+
+		slogans = []
+		slogans.append('Resize any image to any size with few clicks!')
+		slogans.append('The simplest image resizing app!')
+		slogans.append('Just load an image, set the width and height and click on RESIZE ME!')
+
+		index = random.randint(0, len(slogans)-1)
+
+		context = {
+			'slogan': slogans[index],
+		}
+
+		return HttpResponse(template.render(context, request))
+
+	else:
+		
+		template = loader.get_template('resize/index.html')
+
+		slogans = []
+		slogans.append('Resize any image to any size with few clicks!')
+		slogans.append('The simplest image resizing app!')
+		slogans.append('Just load an image, set the width and height and click on RESIZE ME!')
+
+		index = random.randint(0, len(slogans)-1)
+
+		context = {
+			'slogan': slogans[index],
+		}
+
+		return HttpResponse(template.render(context, request))
