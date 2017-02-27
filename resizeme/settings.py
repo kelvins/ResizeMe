3026f26bfe806f6c62e75cb53e2264c587daf909
Kelvin Salton do Prado <kelvinpfw@hotmail.com>
2017-02-26 23:58:01 -0300

    Changes in the settings file

--- resizeme/settings.py
+++ resizeme/settings.py
@@ -115,14 +115,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'resize', 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'resize', 'media')
-MEDIA_URL = '/media/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'resize', 'static'),
+)
 
 # Heroku - https://djangogirls.gitbooks.io/django-girls-tutorial-extensions/heroku/
 
